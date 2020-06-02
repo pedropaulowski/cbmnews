9ba915f4758aa165d5a756f219edc1be74a128cb
Pedro Paulo <ixf34rxi@gmail.com>
2020-06-01 23:31:17 -0300

    correct link

--- public/services/noticias/index.php
+++ public/services/noticias/index.php
@@ -82,7 +82,7 @@ switch($method) {
                         if($noticiaDb->add($noticia)) {
                             http_response_code(200);
                             $reponse = [
-                                "link" => "https://cbm_g1.test/noticia.php?id=$id"
+                                "link" => "http://www.cbmnews.ga/noticia.php?id=$id"
                             ];
 
                             echo json_encode($reponse);
